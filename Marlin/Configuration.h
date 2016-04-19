d77bfac51b94dbadcd9eada548fd0b979f539a88
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2016-04-19 13:51:54 +0200

    Update PID controller working using autotune.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -272,9 +272,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  //#define  DEFAULT_Kp 22.2
+  //#define  DEFAULT_Ki 1.08
+  //#define  DEFAULT_Kd 114
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -285,6 +285,11 @@
   //#define  DEFAULT_Kp 63.0
   //#define  DEFAULT_Ki 2.25
   //#define  DEFAULT_Kd 440
+
+  // gergap's Mendel90 autotune parameter
+  #define  DEFAULT_Kp 83.52
+  #define  DEFAULT_Ki 10.76
+  #define  DEFAULT_Kd 162.16
 
 #endif // PIDTEMP
 
