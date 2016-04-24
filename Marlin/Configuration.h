271d3fa1bd78f99700c7dd332fbe9a0208741f57
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2016-04-24 13:54:40 +0200

    Adjusted Z height, because extruder was remounted better.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -525,7 +525,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 #define Z_MIN_POS 0
 #define X_MAX_POS 200
 #define Y_MAX_POS 200
-#define Z_MAX_POS 181.5
+#define Z_MAX_POS 180.6
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
